97c7660b14bf2166c2a1f4a1cfd7d2d401324a81
liuchunjun <812875535@qq.com>
2019-05-09 09:37:45 +0800

    模板

---
+++ src/components/cbHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">视频课程</el-menu-item>
+      <el-submenu index="2">
+        <el-submenu index="4-4">
+          <template slot="title">开发文档</template>
+          <el-menu-item index="4-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+      <el-menu-item index="4">
+        <el-submenu index="4-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="4-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cbHeader',
+  data () {
+    return {
+    }
+  },
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .c{
+    .d{
+      font-size: 22px;
+      color: red;
+    }
+  }
+</style>
