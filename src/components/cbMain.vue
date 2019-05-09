7279f4f15534da6e0967078dba3d8809da839f15
wutongyu1 <947573532@qq.com>
2019-05-09 23:56:02 +0800

    岗位管理1

--- src/components/cbMain.vue
+++ src/components/cbMain.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>main</div>
+  <div>
+    <Managerial_Position></Managerial_Position>
+  </div>
 </template>
 
 <script>
+  import Managerial_Position  from "./managerial-position-vue/Managerial-Position"
 export default {
-  name: "cbMain"
+  name: "cbMain",
+  components:{
+    Managerial_Position
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+
 </style>
