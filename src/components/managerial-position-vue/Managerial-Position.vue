8fc7b7f5188021e1e278093117311cf8aea45d84
wutongyu1 <947573532@qq.com>
2019-05-12 00:12:53 +0800

    更改了input框的默认样式

--- src/components/managerial-position-vue/Managerial-Position.vue
+++ src/components/managerial-position-vue/Managerial-Position.vue
@@ -1,39 +1,47 @@
 <template>
-    <div class="Managerial_Position">
-      <div class="Ma_Po_Section">
-        <Managerial_Position_Con1></Managerial_Position_Con1>
-        <Managerial_Position_Con2></Managerial_Position_Con2>
-      </div>
+  <div class="Managerial_Position">
+    <div class="Ma_Po_Section">
+      <Managerial_Position_Con1 :data="msg"></Managerial_Position_Con1>
+      <Managerial_Position_Con2 v-on:change="change"></Managerial_Position_Con2>
     </div>
+  </div>
 </template>
 
 <script>
-  import Managerial_Position_Con1 from "./Managerial_Position_Con1"
-  import Managerial_Position_Con2 from "./Managerial_Position_Con2"
-    export default {
-        name: "Managerial_Position",
-      components:{
-        Managerial_Position_Con1,
-        Managerial_Position_Con2
-
-      }
+import Managerial_Position_Con1 from "./Managerial_Position_Con1";
+import Managerial_Position_Con2 from "./Managerial_Position_Con2";
+export default {
+  name: "Managerial_Position",
+  components: {
+    Managerial_Position_Con1,
+    Managerial_Position_Con2
+  },
+  data() {
+    return {
+      msg: true,
+    };
+  },
+  methods: {
+    change() {
+      this.msg = !this.msg;
     }
+  }
+};
 </script>
 
 <style scoped>
-  .Managerial_Position{
-    position:relative;
-    width:100%;
-    height:800px;
-    padding:20px 10px 0 10px;
-    box-sizing: border-box;
-    background:#f3f3f4;
-  }
-  .Ma_Po_Section{
-    width:100%;
-    padding:0 10px;
-    box-sizing: border-box;
-    background-clip: content-box;
-  }
-
+.Managerial_Position {
+  position: relative;
+  width: 100%;
+  height: 800px;
+  padding: 20px 10px 0 10px;
+  box-sizing: border-box;
+  background: #f3f3f4;
+}
+.Ma_Po_Section {
+  width: 100%;
+  padding: 0 10px;
+  box-sizing: border-box;
+  background-clip: content-box;
+}
 </style>
--- src/components/managerial-position-vue/Managerial_Position_Con1.vue
+++ src/components/managerial-position-vue/Managerial_Position_Con1.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="Ma_Po_Se_Con1">
+  <ul class="Ma_Po_Se_Con1" v-if="this.data">
     <el-row :gutter="10">
       <el-col :xs="12" :sm="12" :md="8" :lg="7" :xl="7" style="box-sizing: border-box">
         <div class="grid-content bg-purple grid-content1">
@@ -22,107 +22,120 @@
       <el-col :xs="12" :sm="12" :md="16" :lg="3" :xl="3" style="box-sizing: border-box">
         <div class="grid-content bg-purple-light">
           <el-row id="Ma_Po_Se_Con1_Btn_Con">
-            <el-button type="success" round icon="el-icon-search" class="Ma_Po_Se_Con1_Btn_Search">搜索</el-button>
-            <el-button type="warning" round icon="el-icon-refresh" class="Ma_Po_Se_Con1_Btn_Reset">重置</el-button>
+            <el-button
+              type="success"
+              round
+              icon="el-icon-search"
+              class="Ma_Po_Se_Con1_Btn_Search"
+            >搜索</el-button>
+            <el-button
+              type="warning"
+              round
+              icon="el-icon-refresh"
+              class="Ma_Po_Se_Con1_Btn_Reset"
+            >重置</el-button>
           </el-row>
         </div>
       </el-col>
     </el-row>
   </ul>
-
 </template>
 
 <script>
-    export default {
-        name: "Managerial_Position_Con1",
-      data() {
-        return {
-          options: [{
-            value: '选项1',
-            label: '所有'
-          }, {
-            value: '选项2',
-            label: '正常'
-          }, {
-            value: '选项3',
-            label: '停用'
-          }, ],
-          value: ''
+export default {
+  name: "Managerial_Position_Con1",
+  components: {},
+  props: ["data"],
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "所有"
+        },
+        {
+          value: "选项2",
+          label: "正常"
+        },
+        {
+          value: "选项3",
+          label: "停用"
         }
-      }
-
-    }
+      ],
+      value: ""
+    };
+  }
+};
 </script>
 
 <style scoped>
- .Ma_Po_Se_Con1{
-   width:100%;
-   display: flex;
-   flex-wrap: wrap;
-   border-radius: 6px;
-   border :1px solid #ececed;
-   box-sizing: border-box;
-   background: #ffffff;
-   box-shadow: 2px 1px 3px 0.5px #e1e1e2;
- }
- .grid-content{
-   display: flex;
-   padding-top: 10px;
-   padding-left: 8px;
-   padding-bottom: 18px;
-   box-sizing: border-box;
-   align-items: center;
- }
+.Ma_Po_Se_Con1 {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  border-radius: 6px;
+  border: 1px solid #ececed;
+  box-sizing: border-box;
+  background: #ffffff;
+  box-shadow: 2px 1px 3px 0.5px #e1e1e2;
+}
+.grid-content {
+  display: flex;
+  padding-top: 10px;
+  padding-left: 8px;
+  padding-bottom: 18px;
+  box-sizing: border-box;
+  align-items: center;
+}
 
- el-col{
-   padding-left:0;
-   padding-right: 0;
- }
+el-col {
+  padding-left: 0;
+  padding-right: 0;
+}
 
-  .Ma_Po_Se_Con1_num{
-    white-space: nowrap;
-    font-size:13px;
-    color: #333
-  }
-  .Ma_Po_Se_Con1_input{
-    width:200px;
-    height:30px;
-    outline: none;
-   padding-left: 5px;
-    border:1px solid #dddddd;
-    border-radius: 3px;
-    box-sizing: border-box;
-  }
-  .Ma_Po_Se_Con1_Btn{
-    width:22%;
-    padding-left:15px;
-    padding-top:10px;
-    display: flex;
-    padding-bottom: 18px;
-  }
-  .Ma_Po_Se_Con1_Btn_Search{
-    width:60px;
-    height:30px;
-    display: flex;
-    outline: none;
-    border: none;
-    justify-content: center;
-    background-color:#1ab394;
-  }
-  #Ma_Po_Se_Con1_Btn_Con{
-    display: flex;
-    justify-content: center;
-    text-align: center;
-  }
-  .Ma_Po_Se_Con1_Btn_Reset{
-    width:60px;
-    height:30px;
-    display: flex;
-    outline: none;
-    border: none;
-    justify-content: center;
-    background-color:#f8ac59;
-    text-align: center;
-  }
-
+.Ma_Po_Se_Con1_num {
+  white-space: nowrap;
+  font-size: 13px;
+  color: #333;
+}
+.Ma_Po_Se_Con1_input {
+  width: 200px;
+  height: 30px;
+  outline: none;
+  padding-left: 5px;
+  border: 1px solid #dddddd;
+  border-radius: 3px;
+  box-sizing: border-box;
+}
+.Ma_Po_Se_Con1_Btn {
+  width: 22%;
+  padding-left: 15px;
+  padding-top: 10px;
+  display: flex;
+  padding-bottom: 18px;
+}
+.Ma_Po_Se_Con1_Btn_Search {
+  width: 60px;
+  height: 30px;
+  display: flex;
+  outline: none;
+  border: none;
+  justify-content: center;
+  background-color: #1ab394;
+}
+#Ma_Po_Se_Con1_Btn_Con {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+.Ma_Po_Se_Con1_Btn_Reset {
+  width: 60px;
+  height: 30px;
+  display: flex;
+  outline: none;
+  border: none;
+  justify-content: center;
+  background-color: #f8ac59;
+  text-align: center;
+}
 </style>
